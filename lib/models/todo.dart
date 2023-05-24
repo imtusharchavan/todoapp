732e372ffabf6e3af38c9ffd0b04a6b4903ff03d
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-24 15:21:12 +0530

    Create model todo

---
+++ lib/models/todo.dart
@@ -0,0 +1,13 @@
+class ToDo {
+  final String title;
+  final String content;
+  final bool completed;
+  final DateTime from;
+
+  ToDo({
+    required this.title,
+    required this.content,
+    required this.completed,
+    required this.from,
+  });
+}
