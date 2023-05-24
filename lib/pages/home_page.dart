af348b4b04ec8baa528d5edd3dbde4773fc59f8b
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-24 15:23:27 +0530

    Create list view for todos

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todoapp/models/todo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,6 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<ToDo> todos = [];
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +18,45 @@ class _HomePageState extends State<HomePage> {
         title: const Text("To Do App"),
       ),
       floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {},
+        onPressed: () {},
         label: const Text("Add Todo"),
         icon: const Icon(Icons.add_rounded),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+        ),
+        child: ListView.separated(
+            itemBuilder: (context, index) => ListTile(
+                  title: Text(todos[index].title),
+                  subtitle: Text(todos[index].content),
+                  trailing: IconButton(
+                    onPressed: () {
+                      final todo = todos[index];
+                      setState(() {
+                        todos[index] = ToDo(
+                          title: todo.title,
+                          content: todo.content,
+                          completed: !todo.completed,
+                          from: todo.from,
+                        );
+                      });
+                    },
+                    icon: Icon(
+                      todos[index].completed
+                          ? Icons.check_circle_rounded
+                          : Icons.radio_button_unchecked_rounded,
+                    ),
+                  ),
+                  tileColor: Theme.of(context).colorScheme.primaryContainer,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+            separatorBuilder: (context, index) => const SizedBox(
+            height: 10,
+          ),
+            itemCount: todos.length),
       ),
     );
   }
