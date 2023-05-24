990f2e9cb8294b98308ead5f05b190c5361e523c
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-24 15:35:11 +0530

    Implement encrypting the todo

--- lib/models/todo.dart
+++ lib/models/todo.dart
@@ -1,3 +1,5 @@
+import 'package:encrypt/encrypt.dart';
+
 class ToDo {
   final String title;
   final String content;
@@ -10,4 +12,13 @@ class ToDo {
     required this.completed,
     required this.from,
   });
+
+  Map<String, dynamic> toEncryptJson(Encrypter encrypter, IV iv) {
+    return {
+      "title": encrypter.encrypt(title, iv: iv).base64,
+      "content": encrypter.encrypt(content, iv: iv).base64,
+      "completed": completed,
+      "from": encrypter.encrypt(from.millisecondsSinceEpoch.toString(), iv: iv).base64,
+    };
+  }
 }
