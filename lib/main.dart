73d9670f7b99c0979d34a82887f46b7001a9ee87
Tushar Chavan <tusharrchavan014@gmail.com>
2023-05-24 15:15:49 +0530

    Add dynamic color

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:todoapp/pages/home_page.dart';
 
@@ -8,15 +9,34 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final defaultLightColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+  );
+
+  static final defaultDarkColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+    brightness: Brightness.dark,
+  );
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'To Do App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
+    return DynamicColorBuilder(
+      builder: (lightColorScheme, darkColorScheme) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: lightColorScheme ?? defaultLightColorScheme,
+            useMaterial3: true,
+          ),
+          darkTheme: ThemeData(
+            colorScheme: darkColorScheme ?? defaultDarkColorScheme,
+            useMaterial3: true,
+          ),
+          themeMode: ThemeMode.dark,
+          home: const HomePage(),
+        );
+      },
     );
   }
 }
